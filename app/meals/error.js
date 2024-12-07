a5430ea32a069358bf34a760d42e136ddf95be2a
Wahyu Esya <wahyu.esya17@gmail.com>
2024-12-07 22:44:17 +0700

    add error pages in meals pages

---
+++ app/meals/error.js
@@ -0,0 +1,13 @@
+"use client";
+
+export default function Error({ error }) {
+  console.log({ error });
+  return (
+    <main className="text-center">
+      <h1 className="text-4xl font-extrabold uppercase bg-primary bg-clip-text text-transparent">
+        An error occurred
+      </h1>
+      <p className="mt-4 tetx-lg">{error.message}</p>
+    </main>
+  );
+}
--- app/meals/loading-out.js
+++ app/meals/loading-out.js
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
     <main>
-      <h1 className="animate-pulse text-2xl bg-primary bg-clip-text text-transparent font-semibold">
+      <h1 className="animate-pulse text-2xl bg-primary bg-clip-text text-transparent font-semibold text-center">
         Fetching meals...
       </h1>
     </main>
