255350b94f034bf5d3d4cf48b001bda2b2b0efef
Wahyu Esya <wahyu.esya17@gmail.com>
2024-12-19 11:29:57 +0700

    fix: meals item n grid

--- components/meals/meal-item.js
+++ components/meals/meal-item.js
@@ -8,18 +8,23 @@ export default function MealItem(props) {
     <article className="rounded-lg overflow-hidden bg-stone-800">
       <header>
         <div className="relative h-52 w-full">
-          <Image src={image} alt={id} fill />
+          <Image
+            src={image}
+            alt={id}
+            fill
+            className="object-cover object-center"
+          />
         </div>
         <div className="p-4">
           <h2 className="text-2xl font-semibold tracking-wide bg-clip-text text-transparent bg-primary">
-            {title}
+            {title.length <= 15 ? title : title.slice(0, 15) + "..."}
           </h2>
           <p className="capitalize text-sm text-stone-400">by {creator}</p>
         </div>
       </header>
-      <div className="px-4 pb-4">
+      <div className="px-4 pb-4 min-h-[150px] max-h-[150px] flex  flex-col justify-between">
         <p className="font-light text-balance">
-          {summary.length < 100 ? summary : summary.slice(0, 100) + "..."}
+          {summary.length < 75 ? summary : summary.slice(0, 75) + "..."}
         </p>
         <div className="mt-6 flex justify-end">
           <Link
--- components/meals/meals-grid.js
+++ components/meals/meals-grid.js
@@ -2,7 +2,7 @@ import MealItem from "./meal-item";
 
 export default function MealsGrid({ meals }) {
   return (
-    <ul className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-12">
+    <ul className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-12">
       {meals &&
         meals.map((meal, index) => (
           <li key={index}>
