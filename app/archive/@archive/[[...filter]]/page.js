4518d66cc0f419d18f0f41ab419727831bc9232b
Wahyu Esya <wahyu.esya17@gmail.com>
2024-12-20 15:40:09 +0700

    dealing with multiple path segments

---
+++ app/archive/@archive/[[...filter]]/page.js
@@ -0,0 +1,63 @@
+import NewsList from "@/components/news/news-list";
+import {
+  getAvailableNewsMonths,
+  getAvailableNewsYears,
+  getNewsForYear,
+  getNewsForYearAndMonth,
+  setMonthByNumber,
+} from "@/lib/news";
+import Link from "next/link";
+
+export default function FilteredNewsPage({ params }) {
+  const { filter } = params;
+
+  const selectedYear = filter?.[0];
+  const selectedMonth = filter?.[1];
+
+  let news;
+  let links = getAvailableNewsYears();
+
+  if (selectedYear && !selectedMonth) {
+    news = getNewsForYear(selectedYear);
+    links = getAvailableNewsMonths(selectedYear);
+  }
+
+  if (selectedYear && selectedMonth) {
+    news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+    links = [];
+  }
+
+  let newsContent = <p>No news available for this selected period</p>;
+
+  if (news && news.length > 0) {
+    newsContent = <NewsList news={news} />;
+  }
+
+  return (
+    <>
+      <header>
+        <nav>
+          <ul className="flex gap-6 mb-4">
+            {links.map((link) => {
+              const href = selectedYear
+                ? `/archive/${selectedYear}/${link}`
+                : `/archive/${link}`;
+
+              return (
+                <li key={link}>
+                  <Link
+                    href={href}
+                    className="hover:text-primary text-lg font-semibold transition-all duration-300 ease-in-out text-stone-300"
+                  >
+                    {selectedYear ? setMonthByNumber(link) : link}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </header>
+      {newsContent}
+    </>
+  );
+}
--- app/archive/@archive/[year]/page.js
+++
@@ -1,9 +0,0 @@
-import NewsList from "@/components/news/news-list";
-import { getNewsForYear } from "@/lib/news";
-
-export default function ArchiveYearPage({ params }) {
-  const { year } = params;
-  const newsItem = getNewsForYear(year);
-
-  return <NewsList news={newsItem} />;
-}
