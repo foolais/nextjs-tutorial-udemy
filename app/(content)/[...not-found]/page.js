81a89ea28063ec14fd43c00fd41908532499c906
Wahyu Esya <wahyu.esya17@gmail.com>
2024-12-20 19:30:24 +0700

    add not found in content route group

---
+++ app/(content)/[...not-found]/page.js
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <main className="text-center">
+      <h1 className="text-4xl font-extrabold uppercase text-primary mb-2">
+        Page Not Found
+      </h1>
+      <p>The page you are looking for does not exist</p>
+    </main>
+  );
+}
