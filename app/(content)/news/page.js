cd2d609b6f949b7f7bdabf8fd9e371922bb4c78e
Wahyu Esya <wahyu.esya17@gmail.com>
2024-12-21 16:39:36 +0700

    client-side data fetching

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,15 +1,48 @@
-import { DUMMY_NEWS } from "@/lib/dummy-news";
+"use client";
+
+// import { DUMMY_NEWS } from "@/lib/dummy-news";
 import NewsList from "@/components/news/news-list";
+import { useEffect, useState } from "react";
 
 export default function NewsPage() {
+  const [news, setNews] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchNews() {
+      setIsLoading(true);
+      const response = await fetch("http://localhost:8080/news");
+
+      if (!response.ok) {
+        setError("Failed to fetch news");
+        setIsLoading(false);
+        setNews([]);
+      }
+
+      const data = await response.json();
+      setNews(data);
+      setIsLoading(false);
+    }
+
+    fetchNews();
+  }, []);
+
+  if (isLoading)
+    return <p className="center text-white text-2xl">Loading...</p>;
+  if (error) return <p className="center text-white text-2xl">{error}</p>;
+
+  let newsContent;
+  if (news) {
+    newsContent = <NewsList news={news} />;
+  }
+
   return (
     <>
       <header className="pb-6">
         <h1 className="text-heading">News Page</h1>
       </header>
-      <main>
-        <NewsList news={DUMMY_NEWS} />
-      </main>
+      <main>{newsContent}</main>
     </>
   );
 }
