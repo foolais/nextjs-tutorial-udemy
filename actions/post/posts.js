3d7560739800f169ae1c5afa95d9e7f8fa5b9996
Wahyu Esya <wahyu.esya17@gmail.com>
2024-12-21 22:34:41 +0700

    optimized use server n client form

---
+++ actions/post/posts.js
@@ -0,0 +1,33 @@
+"use server";
+
+const { storePost } = require("@/lib/post");
+const { redirect } = require("next/navigation");
+
+export async function createPost(prevState, formData) {
+  const title = formData.get("title");
+  const image = formData.get("image");
+  const content = formData.get("content");
+
+  let errors = [];
+
+  if (!title || title.trim().length === 0) {
+    errors.push("Title is required!");
+  }
+
+  if (!image || image?.size === 0) errors.push("Image is required!");
+
+  if (!content || content.trim().length === 0) {
+    errors.push("Content is required!");
+  }
+
+  if (errors.length > 0) return { errors };
+
+  await storePost({
+    imageUrl: "",
+    title,
+    content,
+    userId: 1,
+  });
+
+  redirect("/post");
+}
--- app/(content)/post/new-post/page.js
+++ app/(content)/post/new-post/page.js
@@ -1,37 +1,6 @@
+import { createPost } from "@/actions/post/posts";
 import PostForm from "@/components/post/post-form";
-import { storePost } from "@/lib/post";
-import { redirect } from "next/navigation";
 
 export default function NewPostsPage() {
-  async function createPost(prevState, formData) {
-    "use server";
-    const title = formData.get("title");
-    const image = formData.get("image");
-    const content = formData.get("content");
-
-    let errors = [];
-
-    if (!title || title.trim().length === 0) {
-      errors.push("Title is required!");
-    }
-
-    if (!image || image?.size === 0) errors.push("Image is required!");
-
-    if (!content || content.trim().length === 0) {
-      errors.push("Content is required!");
-    }
-
-    if (errors.length > 0) return { errors };
-
-    await storePost({
-      imageUrl: "",
-      title,
-      content,
-      userId: 1,
-    });
-
-    redirect("/post");
-  }
-
   return <PostForm action={createPost} />;
 }
